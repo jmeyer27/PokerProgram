b0584fec0e2ca58c0dd4c5c2e2a9f674b9727b58
jmeyer27 <jasmine6711@rocketmail.com>
2022-11-10 15:46:24 -0800

    Added a few get methods to Card class

--- src/main/java/com/example/pokerprogram/Card.java
+++ src/main/java/com/example/pokerprogram/Card.java
@@ -1,45 +1,70 @@
 package com.example.pokerprogram;
 
+import javafx.scene.image.Image;
+
 public class Card {
 
+//    private final Enum<Card.Rank> Rank;
+//    private final Enum<Card.Suit> Suit;
+    private Image cardImage;
+    private int rank;
+    private int suit;
 
     //todo someone can finish the class :)
     //if you want to implement Suit and Rank differently please feel free to do whatever and delete what I have here
     //~~~~~~~~~~thanks~~~~~~~~~~
 
-    //set up enums
-    enum Suit{
-        HEARTS, CLUBS, DIAMONDS, SPADES
-    };
+//    //set up enums
+//    public enum Suit{
+//        HEARTS, CLUBS, DIAMONDS, SPADES
+//    };
 
-    enum Rank{
-        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6),
-        SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11),
-        QUEEN(12), KING(13), ACE(1);
+//    public enum Rank{
+//        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6),
+//        SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11),
+//        QUEEN(12), KING(13), ACE(1);
+//
+//        final int value;
+//        private Rank(int value){
+//            this.value = value;
+//        }
+//
+//    }
 
-        final int value;
-        private Rank(int value){
-            this.value = value;
-        };
-
-    }
-
-     int rank = 0;
-     int suit = 0;
 
      //constructor for Card class
     public Card(int newRank, int newSuit)
     {
         rank = newRank;
         suit = newSuit;
+        String imageRank = getRank(newRank);
+        String imageSuit = getSuit(newSuit);
+        String fileName = imageRank + "_of_" +imageSuit + ".png";
+        //todo test if this actually returns image
+        cardImage = new Image("/images/pokerCardImages/"+fileName);
+
     }
 
+    //todo test this method
+    private String getSuit(int newSuit) {
+       return switch(newSuit){
+           case 1 -> "hearts";
+           case 2 -> "clubs";
+           case 3 -> "diamonds";
+           case 4-> "spades";
+           default -> "error"; //will cause error hopefully
+       };
+    }
 
-
-
-
-
-
+    //todo test this method
+    private String getRank(int newRank) {
+        return switch (newRank) {
+            case 11 -> "jack";
+            case 12 -> "queen";
+            case 13 -> "king";
+            default -> String.valueOf(newRank);
+        };
+    }
 
 
 }
