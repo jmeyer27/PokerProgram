3e10672569d8beec3fc77099e974ff7af2b5a107
Benjamin Hoang <benjaminh564@gmail.com>
2022-12-13 23:39:52 -0800

    how to page

--- src/main/java/com/example/pokerprogram/controllers/HowToPlayController.java
+++ src/main/java/com/example/pokerprogram/controllers/HowToPlayController.java
@@ -1,4 +1,15 @@
 package com.example.pokerprogram.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class HowToPlayController {
+    @FXML public Button bMenu;
+
+    public void closeMenu(ActionEvent actionEvent){
+        Stage stage = (Stage) bMenu.getScene().getWindow();
+        stage.close();
+    }
 }
